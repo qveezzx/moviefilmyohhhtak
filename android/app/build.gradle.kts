7b7a48d90bbfabfe8593560e5bd8f28a6c9f827c
qveezzx <guydennnooo2@gmail.com>
2026-04-18 18:36:28 -0700

    Fix: Naprawa błędu budowania Androida przy braku kluczy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,16 +42,20 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as? String
+            keyPassword = keystoreProperties["keyPassword"] as? String
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            storePassword = keystoreProperties["storePassword"] as? String
         }
     }
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("release")
+            } else {
+                signingConfig = signingConfigs.getByName("debug")
+            }
 
             ndk {
                 abiFilters.add("arm64-v8a")
